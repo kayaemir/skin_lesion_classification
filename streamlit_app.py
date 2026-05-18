33549474da593fba915860a0fae61681e3506fe9
Remzi Emir Kaya <104939986+kayaemir@users.noreply.github.com>
2026-05-18 14:53:13 +0300

    Update footer

--- streamlit_app.py
+++ streamlit_app.py
@@ -456,10 +456,33 @@ def render_footer():
     st.markdown(
         """
         <div style="text-align:center; color:#555; font-size:0.78rem; margin-top:1.5rem;">
-            &copy; 2025 Graduation Project &nbsp;|&nbsp;
+            &copy; 2026 Graduation Project &nbsp;|&nbsp;
             Skin Lesion Diagnostic Assistant &nbsp;|&nbsp;
             For academic research purposes only &nbsp;|&nbsp;
             Not for clinical use
+            <br><br>
+            <a href="https://github.com/kayaemir/skin_lesion_classification"
+               target="_blank"
+               style="display:inline-flex; align-items:center; gap:6px; color:#888;
+                      text-decoration:none; font-size:0.82rem;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                     viewBox="0 0 24 24" fill="#888">
+                  <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385
+                           .6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235
+                           -3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41
+                           -1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015
+                           1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99
+                           .105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46
+                           -5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53
+                           .12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405
+                           3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23
+                           .66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225
+                           0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81
+                           2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57
+                           A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"/>
+                </svg>
+                kayaemir / skin_lesion_classification
+            </a>
         </div>
         """,
         unsafe_allow_html=True,
@@ -597,3 +620,4 @@ st.markdown(
 
 # Footer
 render_footer()
+
